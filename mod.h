d6bb0f21fd12f12c712bac51c3802f0dd0350bca
Vladimir <47279914+code07734@users.noreply.github.com>
2022-08-12 03:36:28 +0700

    Create mod.h

---
+++ mod.h
@@ -0,0 +1,44 @@
+void mod(u64 *x, u64 div){
+	u64 n=*x,c=1,mask,rem,num,coeff;
+	msu32 hbit;
+	_BitScanReverse64(&hbit,div);
+	hbit+=1;
+	mask=(1<<hbit)-1;
+	coeff=mask+1-div;
+	if(n<(2*div)){c=0;}
+	while(c!=0){
+		rem=n&mask;
+		num=n>>hbit;
+		n=(coeff*num)+rem;
+		if(n<(2*div)){c=0;}
+	}
+	if(n>div){n-=div;}
+	if(n==div){n=0;}
+	
+	*x=n;
+}
+
+void qmod(u64 *x, u64 div){
+	u64 n=*x,c=1,mask,rem,num,coeff;
+	msu32 hbit;
+	_BitScanReverse64(&hbit,div);
+	hbit+=1;
+	mask=(1<<hbit)-1;
+	rem=n&mask;
+	num=(1<<hbit)-div;
+	n>>=hbit;
+	while(c!=0){
+		rem+=num&(0-(n&1));
+		n>>=1;
+		num<<=1;
+		if(num>=div){num-=div;}
+		if(n==0 && rem>=div){
+			n=rem;
+			rem=n&mask;
+			n>>=hbit;
+			num=(1<<hbit)-div;}
+		c=n;
+	}
+	if(rem>=div){rem-=div;}
+	*x=rem;
+}
