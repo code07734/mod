784bc656be4d4f359c5ad197d0fa10c0fb11bd50
Vladimir <47279914+code07734@users.noreply.github.com>
2023-04-19 12:20:59 +0700

    Update mod.h

--- mod.h
+++ mod.h
@@ -1,13 +1,3 @@
-typedef unsigned long long u64;
-typedef unsigned int u32;
-typedef unsigned short int u16;
-typedef unsigned char u8;
-typedef long long i64;
-typedef int i32;
-typedef short int i16;
-typedef char i8;
-typedef unsigned long msu32;
-
 void mod(u64 *x, u64 div){
 	u64 n=*x,c=1,mask,rem,num,coeff;
 	msu32 hbit;
